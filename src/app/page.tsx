6528c52827d0dc36a6ecb076e92d59dc6f4c0a70
Mohammad Kaif <kaifghalib123@gmail.com>
2025-06-04 01:42:50 +0530

    added classes to global

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import Header from "@/components/layout/header";
 import About from "@/components/sections/About";
 import Banner from "@/components/sections/Hero";
 import Blog from "@/components/sections/Blog";
-import CallToAction from "@/components/sections/CallToAction";
 import Instagram from "@/components/sections/Instagram";
 import Services from "@/components/sections/Services";
 import Team from "@/components/sections/Team";
 import Marquee from "@/components/sections/Marquee";
 import WhyUsSection from "@/components/sections/WhyUsSection";
 import StatsSection from "@/components/sections/Counter";
+import TestimonialSection from "@/components/sections/Testimonial";
 
 export default function Page() {
   return (
@@ -23,7 +23,7 @@ export default function Page() {
       <WhyUsSection />
       <StatsSection/>
       <Team />
-      <CallToAction />
+      <TestimonialSection/>
       <Blog />
       <Instagram />
       <Footer />
